060359a36939757f9bff91e115c0dfde0a6e18dd
Kendall Buchanan <kendall@kendagriff.com>
2016-04-23 13:28:41 -0600

    complete solution

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var mongojs = require('mongojs');
+var ObjectId = mongojs.ObjectId;
 
 var app = express();
 app.use(bodyParser.json());
 
 var port = 3000;
+var db = mongojs('birds-3');
+var sightings = db.collection('sightings');
 
 app.post('/api/sighting', function(req, res) {
-  console.log('POST sighting');
-  res.end();
+  sightings.insert(req.body, function(err, sighting) {
+    // if (err) return res.status(500).send(err);
+    res.send(sighting);
+  });
 });
  
 app.get('/api/sighting', function(req, res) {
-  console.log('GET sighting');
-  res.end();
+  sightings.find({status: req.query.status}, function(err, sightings) {
+    res.send(sightings);
+  });
 });
  
 app.delete('/api/sighting', function(req, res) {
-  console.log('DELETE sighting');
-  res.end();
+  sightings.remove({_id: ObjectId(req.query.id)}, function(err, sighting) {
+    res.send(sighting);
+  });
 });
  
 app.put('/api/sighting', function(req, res) {
-  console.log('PUT sighting');
-  res.end();
+  sightings.update({_id: ObjectId(req.query.id)}, {$set: {order: req.body.order}}, function(err, sighting) {
+    res.send(sighting);
+  });
 });
 
 app.listen(port, function() {
