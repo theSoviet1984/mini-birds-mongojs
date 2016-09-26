a2c65e86ea77eb4da4d765c36910e6b3549f63c7
theSoviet1984 <jerad.amundsen@gmail.com>
2016-09-25 23:29:45 -0600

    mini-birds mongo finished

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var mongo = require('mongojs')
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
+var db = mongo('birds');
+var sightings = db.collection("sightings")
 var port = 3000;
 
+
 app.post('/api/sighting', function(req, res) {
-  console.log('POST sighting');
-  res.end();
-});
- 
-app.get('/api/sighting', function(req, res) {
-  console.log('GET sighting');
-  res.end();
-});
- 
-app.delete('/api/sighting', function(req, res) {
-  console.log('DELETE sighting');
-  res.end();
-});
- 
-app.put('/api/sighting', function(req, res) {
-  console.log('PUT sighting');
-  res.end();
+
+  sightings.insert(req.body, function(err, result){
+  if(err){
+    res.status(500).send(err)
+  }
+  res.send(result);
+  })
+
 });
 
+app.get('/api/sighting', function(req, res) {
+ sightings.find(req.query, function(err, result){
+   if(err)res.send(err)
+   res.send(result)
+ })
+});
+
+app.delete('/api/sighting', function(req, res) {
+sightings.remove({_id: mongo.ObjectId(req.query.id)}, function(err, result){
+  if(err)res.send(err)
+  res.send(result)
+})
+});
+
+app.put('/api/sighting', function(req, res) {
+sightings.update({_id: mongo.ObjectId(req.query.id)}, req.body, function(err, result){
+if(err)res.send(err)
+res.send(result)
+});
+})
 app.listen(port, function() {
   console.log("Started server on port", port);
 });
